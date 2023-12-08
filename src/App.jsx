dcd33a338620c9237fc9db4fcecd56774bf59ee1
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2023-12-08 23:17:30 +0330

    set timezone

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Info from "./Components/Info/Info";
 import Detail from "./Components/Detail/Detail";
 import { getCity } from "./utils/api";
+import { Toaster } from "react-hot-toast";
 export default function App() {
   const [search, setSearch] = useState("Tabriz");
   const [isCelsius, setIsCelsius] = useState(false);
@@ -12,8 +13,8 @@ export default function App() {
     name: "",
     country: "",
     temp: "",
-    tempMin: "",
     tempMax: "",
+    tempMin: "",
     feeling: "",
     weather: "",
     weatherDesc: "",
@@ -26,15 +27,15 @@ export default function App() {
   useEffect(() => {
     async function fetchData() {
       const cityInfo = await getCity(search);
-      function operationNum(num) {
-        if (isCelsius) {
-          return (num - 273).toFixed(1);
-        } else {
-          return (1.8 * (num - 273) + 32).toFixed(1);
-        }
-      }
 
-      const date = new Date(cityInfo.dt * 1000);
+      // const date = new Date(cityInfo.dt * 1000);
+      // const utcTime = Date.now();
+      // const localTimeOffset = cityInfo.timezone * 1000;
+      // const date = new Date(utcTime + localTimeOffset);
+
+      const d = new Date();
+      const offset = d.getTimezoneOffset() * 60;
+      const date = new Date((cityInfo.dt + offset + cityInfo.timezone) * 1000);
 
       // Format the date and time
       const options = {
@@ -47,36 +48,13 @@ export default function App() {
 
       const formattedDate = date.toLocaleDateString("en-US", options);
 
-      // function getOrdinalSuffix(day) {
-      //   if (day > 3 && day < 21) return "th";
-      //   switch (day % 10) {
-      //     case 1:
-      //       return "st";
-      //     case 2:
-      //       return "nd";
-      //     case 3:
-      //       return "rd";
-      //     default:
-      //       return "th";
-      //   }
-      // }
-      // const date = new Date((cityInfo.dt + cityInfo.timezone) * 1000);
-      // const options = {
-      //   month: "short",
-      //   day: "numeric",
-      //   hour: "2-digit",
-      //   minute: "2-digit",
-      //   hour12: true,
-      // };
-      // const cityTime = date.toLocaleString("en-US", options);
       setCity({
-        ...city,
         name: cityInfo.name,
         country: cityInfo.sys.country,
-        temp: operationNum(cityInfo.main.temp),
-        tempMax: operationNum(cityInfo.main.temp_max),
-        tempMin: operationNum(cityInfo.main.temp_min),
-        feeling: operationNum(cityInfo.main.feels_like),
+        temp: cityInfo.main.temp,
+        tempMax: cityInfo.main.temp_max,
+        tempMin: cityInfo.main.temp_min,
+        feeling: cityInfo.main.feels_like,
         weather: cityInfo.weather[0].main,
         weatherDesc: cityInfo.weather[0].description,
         windSpeed: cityInfo.wind.speed,
@@ -86,26 +64,11 @@ export default function App() {
       });
     }
     fetchData();
-  }, [search, isCelsius]);
-  // useEffect(() => {
-  //   function operationNum(num) {
-  //     if (isCelsius) {
-  //       return ((parseFloat(num) - 32) / 9.5).toFixed(1); //f->c
-  //     } else {
-  //       return (parseFloat(num) * 9.5 + 32).toFixed(1); //c->f
-  //     }
-  //   }
+  }, [search]);
 
-  //   setCity({
-  //     ...city,
-  //     temp: operationNum(city.temp),
-  //     tempMax: operationNum(city.tempMax),
-  //     tempMin: operationNum(city.tempMin),
-  //     feeling: operationNum(city.feeling),
-  //   });
-  // }, [isCelsius]);
   return (
     <div className=" app">
+      <Toaster />
       <div className="d-flex justify-content-start align-items-center title">
         <img
           className="logo"
@@ -121,7 +84,7 @@ export default function App() {
             isCelsius={isCelsius}
             setSearch={setSearch}
           />
-          <Info search={search} city={city} />
+          <Info isCelsius={isCelsius} city={city} />
           <Detail city={city} />
         </div>
       </div>
--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import "./info.scss";
-export default function Info({ search, city }) {
+export default function Info({ isCelsius, city }) {
+  function operationNum(num) {
+    if (isCelsius) {
+      return (num - 273).toFixed(1);
+    } else {
+      return (1.8 * (num - 273) + 32).toFixed(1);
+    }
+  }
+
   return (
     <div className="my-5 container info rounded-3 p-5">
       <div className="row">
         <div className="col-3">
-          <h2 className="text-white display-1 fw-bold">{city.temp}°</h2>
+          <h2 className="text-white display-1 fw-bold">
+            {operationNum(city.temp)}°
+          </h2>
           <p className="text-white fs-2 ">
             {city.name}, {city.country}
           </p>
         </div>
         <div className="col-3 text-center">
           <h2 className="text-white mb-5 display-4">
-            {city.tempMin}°/{city.tempMax}°
+            {operationNum(city.tempMin)}°/
+            {operationNum(city.tempMax)}°
           </h2>
           <div>
-            <p className="text-white mb-0 fs-5">Feels like {city.feeling}°</p>
+            <p className="text-white mb-0 fs-5">
+              Feels like {operationNum(city.feeling)}°
+            </p>
             <p className="text-white mb-0 fs-5"> {city.timezone}</p>
           </div>
         </div>
